51a8b52ff784bd48360d2b356f9cf57ad8a97698
SB1-rgb <125292154+SB1-rgb@users.noreply.github.com>
2024-11-12 12:15:12 +0200

    Innitial code for implementing Web driver

--- src/main/java/online/contactlistapp/TestRunner.java
+++ src/main/java/online/contactlistapp/TestRunner.java
@@ -1,7 +1,29 @@
 package online.contactlistapp;
 
+
+import online.contactlistapp.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        String currentTabName = driver.getWindowHandle();
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://msign.gov.md/#/");
+        Thread.sleep(5000);
+        driver.close();
+
+
+
+        driver.switchTo().window(currentTabName);
+        driver.get("https://thinking-tester-contact-list.herokuapp.com/login");
+        Thread.sleep(5000);
+        driver.quit();
+
+        System.out.println("The test is fineshed!");
+
     }
 }
