bf553f11aa01af134bbda887d10478a10c2c5146
SB1-rgb <125292154+SB1-rgb@users.noreply.github.com>
2024-11-25 15:12:53 +0200

    Added registration of the user with fake data and log out from the registration page, using page objects

---
+++ src/main/java/online/contactlistapp/pageobjects/AccountPage.java
@@ -0,0 +1,16 @@
+package online.contactlistapp.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountPage extends Page {
+    public AccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickLogoutButton() {
+        logoutButton.click();
+    }
+}
+
